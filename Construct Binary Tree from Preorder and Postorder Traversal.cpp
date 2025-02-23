407b06d3c00481c0d121127d8c0addb0a5a4ee77
Vibhav Gupta <guptavibhav123@gmail.com>
2025-02-23 12:54:35 +0530

    POTD : 23/02/2025

--- Construct Binary Tree from Preorder and Postorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Postorder Traversal.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
+    unordered_map<int,int> postIndex;
     TreeNode* buildTree(int preStart, int preEnd, int postStart, 
                        vector<int>& preorder, vector<int>& postorder)
     {
        if(preStart>preEnd) return NULL;
        if(preStart==preEnd) return new TreeNode(preorder[preStart]);
        int leftSubtreeRoot=preorder[preStart+1];
-       int leftSubtreeSize=1;
-       while(postorder[postStart+leftSubtreeSize-1]!=leftSubtreeRoot) leftSubtreeSize++;
+       int leftSubtreeSize=postIndex[leftSubtreeRoot]-postStart+1;
        TreeNode* root=new TreeNode(preorder[preStart]);
        root->left=buildTree(preStart+1, preStart+leftSubtreeSize, postStart, preorder, postorder);
        root->right=buildTree(preStart+leftSubtreeSize+1, preEnd, postStart+leftSubtreeSize, preorder, postorder);
@@ -26,6 +26,7 @@ public:
     }
 
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        for(int i=0;i<postorder.size();i++) postIndex[postorder[i]]=i;
         return buildTree(0,preorder.size()-1,0,preorder,postorder);
     }
 };
