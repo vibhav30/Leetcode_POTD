976feccd3aa9adcc41210352a1a7a036309c6e17
Vibhav Gupta <guptavibhav123@gmail.com>
2025-02-25 00:09:51 +0530

    POTD : 24/02/2025

--- Most Profitable Path in a Tree.cpp
+++ Most Profitable Path in a Tree.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> adj;
-    vector<int>parent,depth;
-
-    void dfs(int node, int par, int d)
+    vector<int> dBob;
+    int sBob;
+    void dfs(int node, int par, int d, vector<int>& amount, int& ans)
     {
-       depth[node]=d;
-       parent[node]=par;
+       int total=0;
+       if(node==sBob) dBob[node]=0;
+       else dBob[node]=1e5;
+       int maxi=INT_MIN;
        for(auto it : adj[node])
        {
-        if(it!=par) dfs(it,node,d+1);
+        if(it==par) continue;
+        dfs(it,node,d+1,amount,maxi);
+        dBob[node]=min(dBob[node],dBob[it]+1);
        }
-    }
- 
-    int dfs2(int node, int par, vector<int>& amount)
-    {
-       int total=amount[node],ans=INT_MIN;
-       for(auto it : adj[node])
-       {
-        if(it!=par) ans=max(ans,dfs2(it,node,amount));
-       }
-       return ans==INT_MIN?total:total+ans;
+       if(dBob[node]>d) total=amount[node];
+       else if(dBob[node]==d) total=amount[node]/2;
+       ans=max(ans,(maxi==INT_MIN?total:total+maxi));
     }
 
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
         int n=amount.size();
         adj.resize(n);
-        parent.resize(n);
-        depth.resize(n);
+        dBob.resize(n);
+        sBob=bob;
         for(auto it : edges)
         {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        dfs(0,-1,0);
-        int curr=bob,bobDepth=0;
-        while(curr!=-1)
-        {
-           if(depth[curr]>bobDepth) amount[curr]=0;
-           else if(depth[curr]==bobDepth) amount[curr]/=2;
-           curr=parent[curr];
-           bobDepth++;
-        }
-        return dfs2(0,-1,amount);
+        int ans=INT_MIN;
+        dfs(0,-1,0,amount,ans);
+        return ans;
     }
 };
