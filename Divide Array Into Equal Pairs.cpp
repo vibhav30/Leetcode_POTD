d290db9d26a2891fda2099486246c4f2cf1ae5c9
Vibhav Gupta <guptavibhav123@gmail.com>
2025-03-17 22:07:33 +0530

    POTD : 17/03/2025

---
+++ Divide Array Into Equal Pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0; i<nums.size(); i++) mp[nums[i]]++;
+        for(auto it : mp)
+        {
+            if((it.second)&1) return false;
+        }
+        return true;
+    }
+};
