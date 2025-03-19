1b573be4a3b5295c09271373c48b402f4e6e85f5
Vibhav Gupta <guptavibhav123@gmail.com>
2025-03-19 21:43:52 +0530

    POTD : 19/03/2025

---
+++ Minimum Operations to Make Binary Array Elements Equal to One I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums.size(), cnt(0);
+        for(int i=0; i<n-2; i++)
+        {
+            if(nums[i]==0)
+            {
+                nums[i]^=1;
+                nums[i+1]^=1;
+                nums[i+2]^=1;
+                cnt++;
+            }
+        }
+        if(nums[n-1]==1 && nums[n-2]==1 && nums[n-3]==1) return cnt;
+        return -1;
+    }
+};
